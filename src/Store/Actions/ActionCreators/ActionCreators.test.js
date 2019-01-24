c3610178c9b66a31927dd144e21ee7695dea8f61
Amira Atef <amira.atef@bosta.co>
2019-01-24 03:40:57 +0200

    add test for action creator

--- src/Store/Actions/ActionCreators/ActionCreators.test.js
+++ src/Store/Actions/ActionCreators/ActionCreators.test.js
@@ -1,3 +1,12 @@
-it('begin',()=>{
-    
+import {addTasks , updateTaskStatus} from '../ActionTypes'
+describe('addTasks and updateTaskStatus',()=>{
+  it('has the correct type',()=>{
+      const action=addTasks
+      expect(action).toEqual("addTasks")
+  }) ; 
+  it('has the correct type',()=>{
+    const action=updateTaskStatus
+    expect(action).toEqual("updateTaskStatus")
+
+  })
 })
